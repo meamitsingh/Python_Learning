f78b6488a6b81cb330069bfff5f85d1282707dd5
Amit Singh <71982797+meamitsingh@users.noreply.github.com>
2020-11-06 05:52:32 +0530

    Operator Test1
    Arithmetic Operations in Python

---
+++ OperatorTest1.py
@@ -0,0 +1,13 @@
+# To understand Diff Arithmetic operators Single Program
+
+a=10
+b=2
+print("a + b=",a+b)
+print("a - b=",a-b)
+print("a * b=",a*b)
+print("a / b=",a/b)
+print("a % b=",a%b)
+print("a // b=",a//b)
+print("a ** b=",a**b)
+
+
