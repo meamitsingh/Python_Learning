7f4dfe6d6bcda006d1f334e80e2f14bbfa75305b
Amit Singh <71982797+meamitsingh@users.noreply.github.com>
2020-11-20 11:45:30 +0530

    MembershipOper1.py
    To understand membership operator in and not in a Python program

---
+++ MembershipOper1.py
@@ -0,0 +1,4 @@
+#To understand membership operator in python ==> in and not in operator
+list1 = [10,20,30] # define a list
+print (10 in list1) # Checks whether value 10 is in list1
+print (99 not in list1) #checks whether 99 is not there in list1
